ffad7f1918df1e6da6d5435d200fd294c173b399
alexobr <43021027+alexobr@users.noreply.github.com>
2018-09-05 22:08:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current date and time is:");
+            Console.WriteLine(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt"));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
